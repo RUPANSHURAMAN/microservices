9d8abab1a1afa81cb5569e730e77c39c6c7cdc56
rupanshuraman <220330189+omanshu-infoneo@users.noreply.github.com>
2026-02-19 19:51:35 +0530

    Add Constants to seperate file

--- src/main/java/com/example/accounts/controller/AccountsController.java
+++ src/main/java/com/example/accounts/controller/AccountsController.java
@@ -3,13 +3,22 @@ package com.example.accounts.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.accounts.constants.AccountsConstants;
+import com.example.accounts.dto.CustomerDto;
+import com.example.accounts.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+>>>>>>> 7ac1d92 (Add Constants to seperate file)
 
 @RestController
 @RequestMapping(path = "/api/v2")
 public class AccountsController {
 
-    @GetMapping("sayHello")
-    public String sayHello(){
-        return "Ho World";
+    @PostMapping(path = "/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 }
