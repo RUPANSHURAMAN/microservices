0e4cd11fc4f1a9bc61c43c32241c7f790916410f
rupanshuraman <26063397+RUPANSHURAMAN@users.noreply.github.com>
2026-02-20 20:06:01 +0530

    Add Accounts service layer

---
+++ src/main/java/com/example/accounts/service/impl/AccountsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.example.accounts.service.impl;
+
+import com.example.accounts.dto.CustomerDto;
+import com.example.accounts.repository.AccountsRepository;
+import com.example.accounts.repository.CustomerRepository;
+import com.example.accounts.service.IAccountsService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AccountsServiceImpl implements IAccountsService {
+
+    private AccountsRepository accountsRepository;
+
+    private CustomerRepository customerRepository;
+
+    /**
+     *
+     * @param customerDto - CustomerDto Object
+     */
+    @Override
+    public void createAccount(CustomerDto customerDto) {
+
+    }
+}
