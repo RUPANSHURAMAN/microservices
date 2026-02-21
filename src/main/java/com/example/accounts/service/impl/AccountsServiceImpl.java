6a8fb7607e16e69a07a8f561d49f7d21bce198cb
rupanshuraman <26063397+RUPANSHURAMAN@users.noreply.github.com>
2026-02-21 21:59:06 +0530

    Add exception handling for customer already registered with same mobile number

--- src/main/java/com/example/accounts/service/impl/AccountsServiceImpl.java
+++ src/main/java/com/example/accounts/service/impl/AccountsServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.accounts.constants.AccountsConstants;
 import com.example.accounts.dto.CustomerDto;
 import com.example.accounts.entity.Accounts;
 import com.example.accounts.entity.Customer;
+import com.example.accounts.exception.CustomerAlreadyExistException;
 import com.example.accounts.mapper.AccountsMapper;
 import com.example.accounts.mapper.CustomerMapper;
 import com.example.accounts.repository.AccountsRepository;
@@ -29,6 +30,10 @@ public class AccountsServiceImpl implements IAccountsService {
     @Override
     public void createAccount(CustomerDto customerDto) {
         Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        if(customerRepository.findByMobileNumber(customerDto.getMobileNumber()).isPresent()){
+           throw new CustomerAlreadyExistException("Customer already registered with given mobile number"
+           + customerDto.getMobileNumber());
+        }
         Customer savedCustomer = customerRepository.save(customer);
         accountsRepository.save(createNewAccount(savedCustomer));
     }
